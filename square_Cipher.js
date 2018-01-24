5b90af982d310a8e4db7bde18d7b257abf78bf55
Brady <vanderbannond@gmail.com>
2018-01-23 16:18:30 -0800

    added prelim pseudocode

--- square_Cipher.js
+++ square_Cipher.js
@@ -1,2 +1,13 @@
-hey its Carl :)
+// hey its Carl :)
 
+// PSEUDO
+// input: string with other symbols possible
+// output: square of 8 columns 7 rows encoded
+//
+// reg ex keep only letters 54 char length string
+// string to lower case
+//
+// loop over 54 characters
+// every 8 characters new newlines
+// if less than 54 in length, distribute spaces at end of lines evenly to keep block shape
+// When looping, use similar function to simpleCipher encode to encode letters
